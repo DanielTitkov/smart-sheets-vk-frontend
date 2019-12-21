4049d60e1eb19f138265f28d90a48d62d2b24e3c
DanielTitkov <mytasmic@gmail.com>
2019-12-21 06:03:45 +0300

    made SheetTitle editable

--- app/src/components/sheets/SheetTitle.js
+++ app/src/components/sheets/SheetTitle.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from "react";
+import "./SheetTitle.css"
 
 const SheetTitle = props => {
-    const { text } = props;
+    const inputRef = useRef(null);
+    const [inputVisible, setInputVisible] = useState(false);
+    const [text, setText] = useState(props.text);
+
+    function onClickOutSide(e) {
+        // Check if user is clicking outside of <input>
+        if (inputRef.current && !inputRef.current.contains(e.target)) {
+            setInputVisible(false); // Disable text input
+        }
+    }
+
+    useEffect(() => {
+        // Handle outside clicks on mounted state
+        if (inputVisible) {
+            document.addEventListener("mousedown", onClickOutSide);
+        }
+    
+        // This is a necessary step to "dismount" unnecessary events when we destroy the component
+        return () => {
+            document.removeEventListener("mousedown", onClickOutSide);
+        };
+    });
+
+
+
     return (
-        <h1>{ text }</h1>
+        <div className="SheetTitle">
+            {inputVisible ? (
+                <input
+                    ref={inputRef} // Set the Ref
+                    value={text} // Now input value uses local state
+                    onChange={e => {setText(e.target.value);}}
+                />
+            ) : (
+                <h1 onClick={() => setInputVisible(true)}>{text}</h1>
+            )}
+        </div>
     )
 }
 
