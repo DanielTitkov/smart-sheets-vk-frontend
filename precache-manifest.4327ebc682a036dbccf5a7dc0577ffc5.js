8e1575909b597fbd97285a441aae581ebe416f66
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-06-10 20:49:24 +0000

    Deploy DanielTitkov/smart-sheets-vk-frontend to github.com/DanielTitkov/smart-sheets-vk-frontend.git:gh-pages

---
+++ precache-manifest.4327ebc682a036dbccf5a7dc0577ffc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbd84c72c1782a859f8b22d84a256286",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8911333a9a92acb5e35d",
+    "url": "./static/css/2.551d3f2e.chunk.css"
+  },
+  {
+    "revision": "3295ef3dc291d3ef80fe",
+    "url": "./static/css/main.94b6b67d.chunk.css"
+  },
+  {
+    "revision": "8911333a9a92acb5e35d",
+    "url": "./static/js/2.664af756.chunk.js"
+  },
+  {
+    "revision": "3e169c07641d86ad1b71e13ffe5a346f",
+    "url": "./static/js/2.664af756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3295ef3dc291d3ef80fe",
+    "url": "./static/js/main.e681d120.chunk.js"
+  },
+  {
+    "revision": "628eaaaaf82a98dd9226",
+    "url": "./static/js/runtime-main.a708e874.js"
+  }
+]);
